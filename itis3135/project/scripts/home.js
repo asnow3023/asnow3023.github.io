41705f9dd321f5e7a95f011641cba39a4bd375d0
John Lee <asnow3023@gmail.com>
2021-12-05 18:04:57 -0500

    change

---
+++ itis3135/project/scripts/home.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $("#slider").bxSlider({
+        autoStart: true,
+        auto: true,
+        minSlides: 1,
+        maxSlides: 1,
+        slideWidth: 500,
+        slideMargin: 20,
+        randomStart: true,
+        autoDelay: 2000,
+    });
+});
